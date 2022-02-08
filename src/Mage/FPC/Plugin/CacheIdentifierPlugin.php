34c15d33dcdfed11f0c659eb2a02aefd50eadd28
Egor Shitikov <egorshitikov@gmail.com>
2022-02-08 10:07:57 -0800

    Create CacheIdentifierPlugin.php

---
+++ src/Mage/FPC/Plugin/CacheIdentifierPlugin.php
@@ -0,0 +1,37 @@
+?php
+
+namespace Mage\FPC\Plugin;
+
+class CacheIdentifierPlugin
+{
+    /**
+     * Constructor
+     *
+     * @param \Magento\Framework\View\DesignExceptions $designExceptions
+     * @param \Magento\Framework\App\RequestInterface $request
+     * @param \Magento\PageCache\Model\Config $config
+     */
+    public function __construct(
+        \Magento\Framework\View\DesignExceptions $designExceptions,
+        \Magento\Framework\App\RequestInterface $request,
+        \Magento\PageCache\Model\Config $config
+    ) {
+        $this->designExceptions = $designExceptions;
+        $this->request = $request;
+        $this->config = $config;
+    }
+
+    /**
+     * Adds cache key for debug
+     *
+     * @param \Magento\Framework\App\PageCache\Identifier $identifier
+     * @param string $result
+     * @return string
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function afterGetValue(\Magento\Framework\App\PageCache\Identifier $identifier, $result)
+    {
+        @header('M-Key: ' . $result);
+        return $result;
+    }
+}
